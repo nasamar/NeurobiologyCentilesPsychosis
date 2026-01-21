8b5e2e53787903f54c69136bcae7888aa5bec69f
Natalia García-San-Martín <132894441+nasamar@users.noreply.github.com>
2026-01-21 20:07:06 +0100

    Restore initial comments and structure in script

--- Code/NCP_09_parfor_groups_CCA.m
+++ Code/NCP_09_parfor_groups_CCA.m
@@ -7,18 +7,18 @@
 
 % This file is part of Neurobiology Centiles Psychosis toolkit.
 %
-% Neurobiology Centiles Mapping toolkit is free software: 
+% Neurobiology Centiles Psychosis toolkit is free software: 
 % you can redistribute it and/or modify it under the terms of the 
 % GNU General Public License as published by the Free Software Foundation, 
 % either version 3 of the License, or (at your option) any later version.
 %
-% Neurobiology Centiles Mapping toolkit is distributed in the hope that 
+% Neurobiology Centiles Psychosis toolkit is distributed in the hope that 
 % it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
 % warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
 %
 % You should have received a copy of the GNU General Public License
-% along with Neurobiology Centiles Mapping toolkit. If not, see 
+% along with Neurobiology Centiles Psychosis toolkit. If not, see 
 % <https://www.gnu.org/licenses/>.
 
 clear
@@ -145,3 +145,4 @@ elseif strcmp(gca().YLabel.String,'Loading')
 end
 
 writetable(Y_pred_table_groups,[directory,'Y_pred_groups.csv'],'WriteRowNames',true)
+
